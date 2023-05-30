db4c8f92fdf080851a3d65c0a046cbf5994c126c
Max <max@fullimage.net>
2023-05-30 08:55:21 +0100

    encyption 💪💪

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+# !/bin/bash
 
 # preq: `wsl --install Debian`
 # preq: `wsl --export Debian /v/wsl/Debain.tar`
 # preq: `wsl --import cava-wsl ./cava-wsl /v/wsl/Debian.tar`
 # preq: `wsl -d cava-wsl -u max bash -c 'cd $HOME; exec bash'`
-# preq: `nano ~/token` (paste `gh` access token)
 
 # note: Debian doesn't come with `wget` by default, so remote doesn't work?
 # note: copy-paste for now :)
@@ -16,9 +15,6 @@ new_hostname="cava-wsl"
 wsl_username="max"
 
 winscap_path="/mnt/d/winscap.exe"
-
-token_path="~/token"
-remove_token=true
 
 git_name="Cornelius-Figgle"
 git_email="max@fullimage.net"
@@ -47,7 +43,7 @@ EOF
 # note: install programs
 sudo apt update
 sudo apt upgrade -y
-sudo apt install neofetch cava git gh tmux wget cmatrix -y
+sudo apt install neofetch cava git gh tmux wget cmatrix openssl -y
 
 # note: download `winscap` if it doesn't already exist
 if [ ! -f $winscap_path ]; then
@@ -55,7 +51,7 @@ if [ ! -f $winscap_path ]; then
 fi
 
 # note: authenticate `gh` and `git`
-gh auth login --git-protocol https --with-token < $token_path # note: this will read auth token from file (not the most secure, but I can't think of a better way of doing it)
+gh auth login --git-protocol https --with-token < $(read -sp "GH Auth Token: " token ; echo $token | openssl passwd -1 -stdin) # note: this will prompt for auth token (should be pasted in)
 gh auth setup-git
 git config --global user.name $git_name
 git config --global user.email $git_email
@@ -70,19 +66,14 @@ cd $clone_name
 mkdir $(dirname $cava_cfg_path)
 ln -s $clone_name/$(basename $cava_cfg_path) $cava_cfg_path
 
+# note: set bash prompt
 if $simplify_prompt; then
-	# note: set bash prompt
 	(cat <<- EOF
 
 		# note: simplified prompt
 		PS1='\w \$ '
 	EOF
 	) >> $bashrc_path
-fi
-
-if $remove_token; then
-	# note: remove token
-	rm $token_path
 fi
 
 # note: reload shell
