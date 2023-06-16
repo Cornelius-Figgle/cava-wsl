5d6147285c894e893c2b1b29ecce13244a538291
Cornelius-Figgle <max@fullimage.net>
2023-06-16 16:11:23 +0100

    added option for no tmux

--- launch.sh
+++ launch.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-tmux new -s cava-display -d
-tmux set -g status off
+use_tmux=false
 
-# note: cava won't render without this
-# info: https://github.com/karlstav/cava/issues/339
-tmux send-keys -t cava-display:0.0 "TERM=xterm-256color" ENTER
 
 if [ ! -e /tmp/cava.fifo ]; then  # note: if no pipe file
     mkfifo /tmp/cava.fifo
 fi
 
-tmux send-keys -t cava-display:0.0 "/mnt/d/winscap.exe 2 48000 16 > /tmp/cava.fifo &" ENTER
-# old: sleep 1
-tmux send-keys -t cava-display:0.0 "cava" ENTER
+if $use_tmux; then
+	tmux new -s cava-display -d
+	tmux set -g status off
 
-tmux a
+	# note: cava won't render without this
+	# info: https://github.com/karlstav/cava/issues/339
+	tmux send-keys -t cava-display:0.0 "TERM=xterm-256color" ENTER
+
+	tmux send-keys -t cava-display:0.0 "/mnt/d/winscap.exe 2 48000 16 > /tmp/cava.fifo &" ENTER
+	tmux send-keys -t cava-display:0.0 "cava" ENTER
+
+	tmux a
+else
+	/mnt/d/winscap.exe 2 48000 16 > /tmp/cava.fifo &
+	cava
+fi
