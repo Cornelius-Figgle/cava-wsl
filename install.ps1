db940ac7262b14ce501a957026b91872b339265d
Max <max@fullimage.net>
2023-06-30 18:01:24 +0100

    basic container creation

--- install.ps1
+++ install.ps1
@@ -1,1 +1,7 @@
-# hi
+# `wsl --install -d Debian`
+# ctrl-c
+# `wsl --export Debian /v/wsl/Debian-NO_USER.tar`
+# `wsl -d Debian -u root useradd --create-home --user-group --groups  adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev --password "password" max`
+
+wsl --import cava-wsl /v/wsl/cava-wsl /v/wsl/Debian-NO_USER.tar
+wsl -d cava-wsl -u root useradd --create-home --user-group --groups  adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev --password "wsl" cava
