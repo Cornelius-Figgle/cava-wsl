11a688968fddd8c2ad814629b1dce2de1303ad85
Max <max@fullimage.net>
2023-07-12 17:43:48 +0100

    new winscap loc

--- launch.sh
+++ launch.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# note: equivalent to `$env:userprofile\cava-wsl`
-# info: https://superuser.com/a/1391349/1727228
-install_location=$(wslpath {1:-$(cmd.exe /C "echo | set /p _=%USERPROFILE%")\\cava-wsl})
-
 if [ ! -e /tmp/cava.fifo ]; then  # note: if no pipe file
 	mkfifo /tmp/cava.fifo
 fi
@@ -16,5 +12,5 @@ source = /tmp/cava.fifo
 sample_rate = 48000
 EOF 
 
-"$install_location/winscap.exe" 2 48000 16 > /tmp/cava.fifo &
+"/opt/winscap.exe" 2 48000 16 > /tmp/cava.fifo &
 cava
