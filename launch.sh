b61bab0a5d161bf175912c9d04a743e41d32d880
Max <max@fullimage.net>
2023-07-12 16:52:15 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 # note: equivalent to `$env:userprofile\cava-wsl`
 # info: https://superuser.com/a/1391349/1727228
-install_location="$(wslpath $(cmd.exe /C "echo | set /p _=%USERPROFILE%"))/cava-wsl"
+install_location=${1:-"$(wslpath $(cmd.exe /C "echo | set /p _=%USERPROFILE%"))/cava-wsl"}
 
 if [ ! -e /tmp/cava.fifo ]; then  # note: if no pipe file
 	mkfifo /tmp/cava.fifo
