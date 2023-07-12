a2baeb4f84f88ed44ec6ccc496526dea44d48b86
Max <max@fullimage.net>
2023-07-12 17:36:24 +0100

    paths urgh

--- launch.sh
+++ launch.sh
@@ -2,11 +2,18 @@
 
 # note: equivalent to `$env:userprofile\cava-wsl`
 # info: https://superuser.com/a/1391349/1727228
-install_location=${1:-"$(wslpath $(cmd.exe /C "echo | set /p _=%USERPROFILE%"))/cava-wsl"}
+install_location=$(wslpath {1:-$(cmd.exe /C "echo | set /p _=%USERPROFILE%")\\cava-wsl})
 
 if [ ! -e /tmp/cava.fifo ]; then  # note: if no pipe file
 	mkfifo /tmp/cava.fifo
 fi
+
+cat > "~/.config/cava/config" <<- EOF
+[input]
+method = fifo
+source = /tmp/cava.fifo
+sample_rate = 48000
+EOF 
 
 "$install_location/winscap.exe" 2 48000 16 > /tmp/cava.fifo &
 cava
